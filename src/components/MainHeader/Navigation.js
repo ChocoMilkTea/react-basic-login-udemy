a1c73a2f0b3d81f846ee3e22cd682d5c8fec3aec
ChocoMilkTea <hongjun87@gmail.com>
2021-05-19 23:58:06 +0800

    using useContext instead of consumer to retrieve context

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,35 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  const authContext = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(authContext) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {authContext.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {authContext.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {authContext.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        )
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {authContext.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {authContext.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {authContext.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
